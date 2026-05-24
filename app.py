e5920dcd273c7655b246bce63c4167a6d05f4e56
punith <punitiptur2@gmail.com>
2026-05-24 11:18:28 +0530

    Created app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import boto3
+
+# Create EC2 client
+ec2 = boto3.client('ec2', region_name='us-east-1')
+
+# Launch EC2 instance
+response = ec2.run_instances(
+    ImageId='ami-0c02fb55956c7d316',   # Amazon Linux 2 AMI
+    InstanceType='t2.micro',
+    KeyName='my-key-pair',
+    MinCount=1,
+    MaxCount=1
+)
+
+# Print Instance ID
+instance_id = response['Instances'][0]['InstanceId']
+
+print("EC2 Instance Created:", instance_id)
